1eb110ca07ea40cbc10c1c3fa4f42f8e3fbe92ab
jbuendia1y <jgamer669@gmail.com>
2024-01-06 11:57:15 -0500

    Habilitando el MockUsersRepository

--- api/tests/mocks/repositories/users.repository.ts
+++ api/tests/mocks/repositories/users.repository.ts
@@ -1,7 +1,7 @@
-// import { CreateUser } from "../../../src/models/user/create-user.model.ts";
-// import { User, IUser } from "../../../src/models/user/user.model.ts";
-// import { UsersRepository } from "../../../src/repositories/users/users.repository.ts";
-import { UserMock } from "../data/user.ts";
+import { CreateUser } from "../../../src/models/user/create-user.model.ts";
+import { User, IUser } from "../../../src/models/user/user.model.ts";
+import { UsersRepository } from "../../../src/repositories/users/users.repository.ts";
+import { UserMock, generateUserMock } from "../data/user.ts";
 
 export class UserRepositoryForAuthMock {
   findOne(_id: string) {
@@ -9,18 +9,45 @@ export class UserRepositoryForAuthMock {
   }
 }
 
-/* export class MockUsersRepository implements UsersRepository {
+export class MockUsersRepository implements UsersRepository {
+  data: User[] = [
+    generateUserMock(),
+    generateUserMock(),
+    generateUserMock(),
+    generateUserMock(),
+    generateUserMock(),
+  ];
+
   findOne(id: string): Promise<User | null> {
-    throw new Error("Method not implemented.");
+    return Promise.resolve(this.data.find((v) => v.values.id === id) ?? null);
   }
   findOneByUsername(username: string): Promise<User | null> {
-    throw new Error("Method not implemented.");
+    return Promise.resolve(
+      this.data.find((v) => v.values.username === username) ?? null
+    );
   }
   save(data: CreateUser): Promise<void> {
-    throw new Error("Method not implemented.");
+    this.data.push(
+      new User({
+        id: crypto.randomUUID(),
+        avatar: null,
+        createdAt: new Date(),
+        isAdmin: false,
+        updatedAt: new Date(),
+        ...data.values,
+      })
+    );
+    return Promise.resolve();
   }
   update(id: string, data: Partial<IUser>): Promise<void> {
-    throw new Error("Method not implemented.");
+    const idx = this.data.findIndex((v) => v.values.id === id);
+
+    this.data[idx] = new User({
+      ...this.data[idx].values,
+      ...data,
+      updatedAt: new Date(),
+    });
+
+    return Promise.resolve();
   }
 }
- */
